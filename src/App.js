e653960164edd1ed2aa108cfd1a5a27a645d85f9
Sara Pulido <sara01rizo@gmail.com>
2021-11-13 00:34:15 +0100

    Added Bootstrat Dependency v 5.1.3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Field from "./components/field";
-import Languages from "./components/lenguages";
+import Languages from "./components/languages";
 import Translate from "./components/translate";
 
 export default function App() {
