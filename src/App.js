4adceaaf5137d3437d9e56ec9a582ad6bef3a69c
Sara Pulido <sara01rizo@gmail.com>
2021-11-12 23:32:11 +0100

    Update useState statement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Field from "";
+import Field from "./components/field";
+import Languages from "./components/lenguages";
+import Translate from "./components/translate";
 
 export default function App() {
+  const [language, setLanguage] = useState("ru");
+  const [text, setText] = useState("");
   return (
     <div>
-      <h1 className="title"> Hi from react task! </h1>
+      <Field label="Enter English" onChange={setText} value={text} />
+      <Languages languages={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
     </div>
   );
 }
