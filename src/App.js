e0a84b21a0376fdfb41933d7043317a0be95a34b
Sara Pulido <sara01rizo@gmail.com>
2021-11-12 23:18:20 +0100

    Init react task project structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Field from "";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="title"> Hi from react task! </h1>
+    </div>
+  );
+}
